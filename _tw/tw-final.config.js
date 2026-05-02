e61c157ffa419ab237b66c8695156b3aa2179f1f
DannySteven09 <144863251+DannySteven09@users.noreply.github.com>
2026-05-02 11:09:47 -0600

    Initial commit

---
+++ _tw/tw-final.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        brand: '#FE5000',
+        'brand-dark': '#E04600',
+        dark: { 400: '#818181', 500: '#524C4A', 900: '#070200' }
+      }
+    }
+  }
+}
